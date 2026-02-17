00c3852d181c7a249ba3d902f0a8de03e3d7f138
antonarhipov <ant.arhipov@gmail.com>
2026-02-17 15:30:21 +0200

    Remove unused Netty dependencies from build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,9 +32,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    implementation(platform("io.netty:netty-bom:4.1.108.Final"))
-    implementation("io.netty:netty-all")
 }
 
 kotlin {
